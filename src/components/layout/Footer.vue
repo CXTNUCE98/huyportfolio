b3a6dd5e3d446c70cbbe0e5bb10f8c0f9441dbfc
taocx <taocx@gviet.vn>
2026-06-09 00:17:51 +0700

    update filter

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -12,20 +12,16 @@ const scrollToTop = () => {
       <img src="/images/back-to-top-bg-5ac316.png" alt="" class="absolute inset-0 w-full h-full object-cover" />
     </div>
 
-    <div class="max-w-[1080px] mx-auto px-4 lg:px-0 pt-10 lg:pt-10 pb-6 lg:pb-10">
-      <div class="hidden lg:flex items-center justify-between">
+    <div class="max-w-[1080px] mx-auto px-4 lg:px-0 py-6 lg:py-0 lg:h-[90px] lg:flex lg:items-center">
+      <div class="hidden lg:flex items-center justify-between w-full">
         <p class="text-sm text-[#7F7F7F]">&copy; 2026 Portfolio's Hyu</p>
         <div class="flex items-center gap-6">
-          <!-- <a href="mailto:Dangquochuy2406@gmail.com"
-            class="text-sm text-[#7F7F7F] transition-colors">Dangquochuy2406@gmail.com</a> -->
-          <div class="text-sm text-[#7F7F7F] transition-colors">Dangquochuy2406@gmail.com</div>
-          <!-- <a href="tel:0347000678" class="text-sm text-[#7F7F7F] hover:text-[#333] transition-colors">0347.000.678</a> -->
-          <div class="text-sm text-[#7F7F7F]transition-colors">0347.000.678</div>
-          <!-- <a href="#" class="text-sm text-[#7F7F7F] hover:text-[#333] transition-colors">Facebook</a> -->
+          <span class="text-sm text-[#7F7F7F]">Dangquochuy2406@gmail.com</span>
+          <span class="text-sm text-[#7F7F7F]">0347.000.678</span>
         </div>
       </div>
 
-      <div class="lg:hidden flex flex-col gap-6">
+      <div class="lg:hidden">
         <p class="text-sm text-[#7F7F7F]">&copy; 2026 Portfolio's Hyu</p>
       </div>
     </div>
